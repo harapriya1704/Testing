c938d8691eaf04a7262d34045b2086f86828d64c
harapriya1704 <123739913+harapriya1704@users.noreply.github.com>
2025-07-06 15:44:14 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent
+
+INPUT_DIR = BASE_DIR / "input"
+PROCESSED_DIR = BASE_DIR / "processed"
+OUTPUT_DIR = BASE_DIR / "output"
+
+
+GLASSBOX_URL = "https://glassbox.dell.com/webinterface/webui/sessions"
+WAIT_TIMES = {
+    "PAGE_LOAD": 40,
+    "GIA_LOAD": 25,
+    "SHORT": 10,
+    "COOKIE_EXTRACTION": 20
+}
+
+
+for directory in [INPUT_DIR, PROCESSED_DIR, OUTPUT_DIR]:
+    directory.mkdir(exist_ok=True)
